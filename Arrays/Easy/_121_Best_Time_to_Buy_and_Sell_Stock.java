3d41d27400d100798d4f48ce6cc0f29683021d09
Aryan Baadlas <154962185+be-a-guptaji@users.noreply.github.com>
2025-06-21 23:10:37 +0530

    121. Best Time to Buy and Sell Stock Solved

--- Arrays/Easy/_121_Best_Time_to_Buy_and_Sell_Stock.java
+++ Arrays/Easy/_121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -61,7 +61,7 @@ public class _121_Best_Time_to_Buy_and_Sell_Stock {
                 maxProfit = profit;
             }
         }
-        // Return the maximum profit found
+        // Return the maximum profit
         return maxProfit;
     }
 
