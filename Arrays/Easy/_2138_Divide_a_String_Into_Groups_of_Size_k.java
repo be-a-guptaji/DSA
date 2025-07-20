dd035e0642c84705de57ab4c73506d31d2d20f04
Aryan Baadlas <154962185+be-a-guptaji@users.noreply.github.com>
2025-07-21 01:26:14 +0530

    2138. Divide a String Into Groups of Size k

--- Arrays/Easy/_2138_Divide_a_String_Into_Groups_of_Size_k.java
+++ Arrays/Easy/_2138_Divide_a_String_Into_Groups_of_Size_k.java
@@ -11,10 +11,7 @@ Note that the partition is done so that after removing the fill character from t
 
 Given the string s, the size of each group k and the character fill, return a string array denoting the composition of every group s has been divided into, using the above procedure.
 
-
-
 Example 1:
-
 Input: s = "abcdefghi", k = 3, fill = "x"
 Output: ["abc","def","ghi"]
 Explanation:
@@ -23,15 +20,14 @@ The next 3 characters "def" form the second group.
 The last 3 characters "ghi" form the third group.
 Since all groups can be completely filled by characters from the string, we do not need to use fill.
 Thus, the groups formed are "abc", "def", and "ghi".
-Example 2:
 
+Example 2:
 Input: s = "abcdefghij", k = 3, fill = "x"
 Output: ["abc","def","ghi","jxx"]
 Explanation:
 Similar to the previous example, we are forming the first three groups "abc", "def", and "ghi".
 For the last group, we can only use the character 'j' from the string. To complete this group, we add 'x' twice.
 Thus, the 4 groups formed are "abc", "def", "ghi", and "jxx".
-
 
 Constraints:
 
