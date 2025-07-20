a895ff2a37fb1bde48c840094e0815ff559ce47a
Aryan Baadlas <154962185+be-a-guptaji@users.noreply.github.com>
2025-07-21 01:21:44 +0530

    125. Valid Palindrome

--- Arrays/Easy/_125_Valid_Palindrome.java
+++ Arrays/Easy/_125_Valid_Palindrome.java
@@ -7,25 +7,21 @@ Problem Statement: A phrase is a palindrome if, after converting all uppercase l
 
 Given a string s, return true if it is a palindrome, or false otherwise.
 
-
-
 Example 1:
-
 Input: s = "A man, a plan, a canal: Panama"
 Output: true
 Explanation: "amanaplanacanalpanama" is a palindrome.
-Example 2:
 
+Example 2:
 Input: s = "race a car"
 Output: false
 Explanation: "raceacar" is not a palindrome.
-Example 3:
 
+Example 3:
 Input: s = " "
 Output: true
 Explanation: s is an empty string "" after removing non-alphanumeric characters.
 Since an empty string reads the same forward and backward, it is a palindrome.
-
 
 Constraints:
 
@@ -43,7 +39,6 @@ Space Complexity: O(1), You're not using extra space apart from variables (left,
 package Arrays.Easy;
 
 public class _125_Valid_Palindrome {
-
     // Method to check if a string is a palindrome
     public static boolean isPalindrome(String s) {
         // Make Character Array for the string
